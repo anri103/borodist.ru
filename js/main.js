ea56e2a15c7df9e1d6f816d64432dd2f576660b7
anri103 <ianrianri@gmail.com>
2023-01-30 16:48:18 +0300

    add cart page

--- js/main.js
+++ js/main.js
@@ -1,30 +1,73 @@
 (function ($) {
     'use strict';
 
+    // $(document).ready(function () {
+    //     $('.quantity-input .button-minus').click(function () {
+    //         var $input = $(this).parent().find('#cont');
+    //         var count = parseInt($input.val()) - 1;
+    //         count = count < 1 ? 1 : count;
+    //         $input.val(count);
+    //         $input.change();
+    //         return false;
+    //     });
+    //     $('.quantity-input .button-plus').click(function () {
+    //         var $input = $(this).parent().find('#cont');
+    //         $input.val(parseInt($input.val()) + 1);
+    //         $input.change();
+    //         return false;
+    //     });
+    // });
+
     $(document).ready(function () {
-        $('.product-details .button-minus').click(function () {
-            var $input = $(this).parent().find('#cont');
-            var count = parseInt($input.val()) - 1;
-            count = count < 1 ? 1 : count;
-            $input.val(count);
+        $('body').on('click', '.number-minus, .number-plus', function () {
+            var $row = $(this).closest('.number-quantity');
+            var $input = $row.find('.number-text');
+            var step = $row.data('step');
+            var val = parseFloat($input.val());
+            if ($(this).hasClass('number-minus')) {
+                val -= step;
+            } else {
+                val += step;
+            }
+            $input.val(val);
             $input.change();
             return false;
         });
-        $('.product-details .button-plus').click(function () {
-            var $input = $(this).parent().find('#cont');
-            $input.val(parseInt($input.val()) + 1);
-            $input.change();
-            return false;
+
+        $('body').on('change', '.number-text', function () {
+            var $input = $(this);
+            var $row = $input.closest('.number-quantity');
+            var step = $row.data('step');
+            var min = parseInt($row.data('min'));
+            var max = parseInt($row.data('max'));
+            var val = parseFloat($input.val());
+            if (isNaN(val)) {
+                val = step;
+            } else if (min && val < min) {
+                val = min;
+            } else if (max && val > max) {
+                val = max;
+            }
+            $input.val(val);
         });
     });
 
-    $('.productCardSwiper-prev1').click(function (e) {
+    $('#productCardSwiper-prev1').click(function (e) {
         e.preventDefault()
         productCardSwiper.slidePrev()
     });
-    $('.productCardSwiper-next1').click(function (e) {
+    $('#productCardSwiper-next1').click(function (e) {
         e.preventDefault()
         productCardSwiper.slideNext()
+    });
+
+    $('#productCardSwiper2-prev1').click(function (e) {
+        e.preventDefault()
+        productCardSwiper2.slidePrev()
+    });
+    $('#productCardSwiper2-next1').click(function (e) {
+        e.preventDefault()
+        productCardSwiper2.slideNext()
     });
 
 })(jQuery);
@@ -76,6 +119,28 @@ const productCardSwiper = new Swiper('.productCardSwiper', {
             grid: {
                 rows: 2,
             },
+        },
+    },
+});
+
+const productCardSwiper2 = new Swiper('.productCardSwiper2', {
+    slidesPerView: 2,
+    grabCursor: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+    navigation: {
+        nextEl: '.productCardSwiper-next',
+        prevEl: '.productCardSwiper-prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+        },
+        992: {
+            slidesPerView: 4,
         },
     },
 });
